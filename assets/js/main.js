7684961c744ed83fa51488722332beb35d114c91
Joshua Cook <jrcook1@mail.wvu.edu>
2017-04-26 13:10:20 -0500

    Updating with what we completed in the workshop

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,28 +1,41 @@
-var app = new Vue({
+var vm = new Vue({
 	el: '#app',
 	data: {
 		lists: [
 			{
-				title: 'Something', 
-				isActive: true, 
+				title: 'To Do', 
+				isActive: true,
+				isEditing: false,
 				todos: [
 					{
-						title: 'Something',
-						isComplete: false
+						title: 'Todo 1',
+						isCompleted: false, 
+						isEditing: false
+					}, 
+					{
+						title: 'Todo 2', 
+						isCompleted: false, 
+						isEditing: false
 					}
 				]
 			}, 
 			{
-				title: 'Something else', 
-				isActive: false, 
+				title: 'New Todo list',
+				isActive: false,
+				isEditing: false,
 				todos: []
 			}
 		]
 	}, 
+	watch: {
+		lists: {
+			handler: function() {
+				// save the data
+			}, 
+			deep: true
+		}
+	},
 	computed: {
-		activeList: function() {
-
-		},
 		activeLists: function() {
 			return this.lists.filter( function( list ) {
 				return list.isActive;
@@ -30,18 +43,55 @@ var app = new Vue({
 		}
 	},
 	methods: {
-		activateList: function( listIndex ) {
+		setActiveList: function( listIndex ) {
+			console.log( event );
+			console.log( 'I clicked on ' + listIndex );
 			for( var i = 0; i < this.lists.length; i++ ) {
-				this.lists[i].isActive = ( listIndex === i ) ? true : false ;
+				if( i === listIndex ) {
+					this.lists[i].isActive = true;
+				} else {
+					this.lists[i].isActive = false;
+				}
 			}
 		}, 
-		addList: function() {
-			this.lists.push( {
-				title: 'Untitled List', 
+		addNewList: function() {
+			var length = this.lists.push({
+				title: 'Untitled List',
 				isActive: false,
+				isEditing: false,
 				todos: []
 			});
-			this.activateList( this.lists.length - 1 );
+			this.setActiveList( length - 1 );
+		}, 
+		addNewTodo: function() {
+			this.activeLists[0].todos.push({
+				title: '',
+				isCompleted: false, 
+				isEditing: true
+			});
+		}, 
+		deleteList: function( listIndex ) {
+			console.log( 'deleted list index' );
+			this.setActiveList( this.lists.length - 1 );
 		}
+	}, 
+	mounted: function() {
+		var $this = this;
+		jQuery( function( $ ) {
+			$( $this.$el );
+		});
 	}
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
